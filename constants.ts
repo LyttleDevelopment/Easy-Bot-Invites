d946709f26f668223ad238cff7de7db5a04fd148
Stualyttle Kirry <Stualyttle@users.noreply.github.com>
2024-10-08 20:41:51 +0200

    cleanup

--- constants.ts
+++ constants.ts
@@ -1,9 +1,2 @@
-/**
- * The developer's ids.
- * This is used to message the devs when an error occurs.
- */
-export const JEF_ID = '548240698869284884';
-export const FAMILY_FEED_CHANNEL_ID = '1141034080520130612';
-export const FRIENDS_FEED_CHANNEL_ID = '1141034151882002572';
 export const KICK_TIME = 5 * 60 * 60 * 1000; // 5 hours
 // export const KICK_TIME = 5 * 60 * 1000; // 5 minutes
