4d2addceb5c977360807f1ed786121562663d8a8
Stualyttle Kirry <Stualyttle@users.noreply.github.com>
2024-10-19 21:35:10 +0200

    Add docker image & workflow

--- src/utils/get-jef-user.ts
+++
@@ -1,9 +0,0 @@
-import client from '../main';
-import { JEF_ID } from '../../constants';
-
-export function getJefUser() {
-  const userId = JEF_ID;
-  // get user from client
-  const user = client.users.cache.get(userId);
-  return user;
-}
